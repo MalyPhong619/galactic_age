021ea2c63683fca1b256e63e70b78280ce607377
Maly Phongsavanh <phongsavanh619@icloud.com>
2019-03-15 08:29:19 -0700

    set up webpacks in package.json, add .gitignore, and webpack.config.js

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/scss/bootstrap.scss';
+import './scss/styles.scss';
+
+$(document).ready(function() {
+  console.log("hey, you should buy Abe a beer sometime!");
+});
---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  devtool: 'eval-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+
+  plugins: [
+    new UglifyJsPlugin({ sourceMap: true }),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Project Title',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(gif|png|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-srcsets-loader',
+        options: {
+          attrs: ['img:src', ':srcset'],
+        },
+      },
+
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      },
+      {
+        test: /\.js$/,
+        exclude: [
+          /node_modules/,
+          /spec/
+        ],
+        loader: "babel-loader",
+        options: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  }
+};
